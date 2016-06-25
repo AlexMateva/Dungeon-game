d394f3c37773704ad22916cb2c67a83af81ec629
Alexandra Mateva <alex.mateva@gmail.com>
2016-06-25 21:11:15 +0300

    Added recover and info.

--- dungeon.pl
+++ dungeon.pl
@@ -1,110 +1,75 @@
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 use utf8;
-
 use v5.020;
+
 use List::Util qw(shuffle);
 use Switch;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use PPP::Hero;
+use PPP::Enemy;
+
 
 local $, = ' ';
 
 
-package Enemy{
-	use Moose;
-	has 'name' => (is => 'rw', isa => 'Str');
-	has 'power' => (is => 'rw', isa => 'Int');
-	has 'health' => (is => 'rw', isa => 'Int');	
-}
-
-package Hero{
-	use Moose;
-
-	has 'name' => (is => 'rw', isa => 'Str');
-	has 'health' => (is => 'rw', isa => 'Int');
-	has 'start_health' => (is => 'rw', isa => 'Int');
-	has 'weapon' => (is => 'rw', isa => 'Str');
-	has 'weapon_power' => (is => 'rw', isa => 'Int');
-}
-
-
-my $hero = Hero->new(
+my $hero = PPP::Hero->new(
 	name => 'Bron',
 	health => 50,
 	start_health => 100,
 	weapon => 'Nails',
 	weapon_power => 10
-	);
+);
 
-my $enemy = Enemy->new(
+my $enemy = PPP::Enemy->new(
 	name => 'Cat',
 	health => 70,
-	power => 40);
+	power => 10
+);
 
-my $enemy2 = Enemy->new(
+my $enemy2 = PPP::Enemy->new(
 	name => 'Bear',
 	health => 80,
-	power => 50);
-
-state @board;
-state $level;
-state @hero;
-@hero = (0,0, $hero);
-
-my @weapon0 = ('Axe', 30);
-my @weapon1 = ('Bow', 40);
-state @weapons;
-@weapons = ('Axe', 30, 'Bow', 40);
-
-our @enemies;
-@enemies = ($enemy, $enemy2);
+	power => 50
+);
 
 our %treasuries = (
-	0 => \&recover,
+	0 => \&health,
 	1 => \&take_new_weapon
-	);
+);
 
 our %commands = (
 	up => \&go_up,
 	left => \&go_left,
 	right => \&go_right,
 	down => \&go_down,
-	help => \&helper
-	);
+	help => \&helper,
+	info => \&info
+);
+
+my @board;
+my $level;
+my @hero;
+my @weapons;
+my @enemies;
+
+@hero = (0,0, $hero);
+@weapons = ('Axe', 30, 'Bow', 40);
+@enemies = ($enemy, $enemy2);
 
 
-say "write 'exit' for end of the game";
-say "Choose level:\neasy\nmedium\nhard\n";
-my $i = 0;
-while (<STDIN>) {
-    chomp;
 
-    next unless length;
-    last if $_ =~ /exit/i;
-    
-    if ($i == 0)
-    {
-    	next unless $_ =~ /easy/i || $_ =~ /medium/i || $_ =~ /hard/i;
-    	switch($_){
-    		case 'easy' {$level = 1}
-    		case 'medium' {$level = 2}
-    		case 'hard' {$level = 3}
-    	}
-    	@board = generate_table();
-    	print_board(@board);
-    	$i += 1;
-    }
-    elsif($i > 1){
-    	next unless defined $commands{$_};
-	    $commands{$_}->();
-	}
-   	say "Enter command";
-    $i += 1;
-}
+
+
 
 sub generate_table
 {
-	my @matrx = ('.', 'T', '.', 'X', 'X', 'X', '.', 'X', '.', 'T', '.', 'E', '.');
-	# my @matrx = ('E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E');
+	my @matrx = ('.', 'T', '.', '.', 'X', 'X', '.', 'X', '.', 'T', '.', 'E', '.');
 	my @matrix;
 	if ($level == 1){
 		@matrx = shuffle @matrx;
@@ -120,7 +85,7 @@ sub generate_table
 		push @matrix, (\@row0, \@row1, \@row2, \@row3, \@row4);
 	}
 	elsif($level == 2){
-		push @matrx, ('E', 'T', 'X', 'X', 'X', '.', '.', '.', '.');
+		push @matrx, ('E', 'T', '.', 'X', 'X', '.', '.', '.', '.');
 		@matrx = shuffle @matrx;
 		my @row0 = @matrx[0..2];
 		unshift @row0, 'H';
@@ -133,7 +98,7 @@ sub generate_table
 		push @matrix, (\@row0, \@row1, \@row2, \@row3, \@row4, \@row5);
 	}
 	elsif($level == 3){
-		push @matrx, ('E', 'E', 'E', 'T', 'T', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', '.', '.', '.', '.', '.', '.', '.');
+		push @matrx, ('E', 'E', 'E', 'T', 'T', 'X', '.', 'X', '.', 'X', 'X', '.', '.', '.', '.', '.', '.', '.', '.', '.');
 		@matrx = shuffle @matrx;
 		my @row0 = @matrx[0..3];
 		unshift @row0, 'H';
@@ -150,13 +115,14 @@ sub generate_table
 }
 
 sub helper{
-	my $info;
-	say $info;
+	say "'exit' - end of game\nIf the move is possible you can use 'up', 'down', 'right' or 'left' to move in one of this directions.";
+	say "Type 'info' for more information about you.";
 }
 
 sub print_board{
 	my @table = @_;
 	my $j = 0;
+	print "\n";
 	while ( $j < 0+@table){
 		foreach my $element ( @{ $table[$j] } ) {
 		    print $element, " ";
@@ -164,19 +130,22 @@ sub print_board{
 		print "\n";
 	$j += 1;
 	}
+	say "\n";
 }
 
 sub go_left{
 	if($hero[1] > 0 && $board[$hero[0]][$hero[1] -1] ne 'X'){
 		take_treasury($hero[0],$hero[1] - 1);
+		won_game($hero[0] + 1, $hero[1] - 1);
 		if (battle($hero[0], $hero[1] - 1) == 0){
 			say "You lost the game";
-			die;
+			exit;
 		}
 		$board[$hero[0]][$hero[1]] = '.';
 		$board[$hero[0]][$hero[1] - 1] = 'H';
 		@hero[1] = $hero[1] - 1;
-	print_board(@board);
+		recover();
+		print_board(@board);
 	}
 	else {
 		say "This move is not possible.\nTry again.";
@@ -192,13 +161,15 @@ sub go_right{
 	}
 	if($hero[1] < $end_of_board && $board[$hero[0]][$hero[1] +1] ne 'X'){
 		take_treasury($hero[0],$hero[1] + 1);
+		won_game($hero[0], $hero[1] + 1);
 		if (battle($hero[0], $hero[1] + 1) == 0){
 			say "You lost the game";
-			die;
+			exit;
 		}
 		$board[$hero[0]][$hero[1]] = '.';
 		$board[$hero[0]][$hero[1] +1] = 'H';
 		@hero[1] = $hero[1] + 1;
+		recover();
 		print_board(@board);
 	}
 	else {
@@ -210,13 +181,15 @@ sub go_right{
 sub go_up{
 	if($hero[0] > 0 && $board[$hero[0] - 1][$hero[1]] ne 'X'){
 		take_treasury($hero[0] - 1,$hero[1]);
+		won_game($hero[0] - 1, $hero[1]);
 		if (battle($hero[0] - 1, $hero[1]) == 0){
 			say "You lost the game";
-			die;
+			exit;
 		}
 		$board[$hero[0]][$hero[1]] = '.';
 		$board[$hero[0] - 1][$hero[1]] = 'H';
 		@hero[0] = $hero[0] - 1;
+		recover();
 		print_board(@board);
 	}
 	else{
@@ -234,13 +207,15 @@ sub go_down{
 	}
 	if($hero[0] < $end_of_board && $board[$hero[0] + 1][$hero[1]] ne 'X'){
 		take_treasury($hero[0] + 1, $hero[1]);
+		won_game($hero[0] + 1, $hero[1]);
 		if (battle($hero[0] + 1, $hero[1]) == 0){
 			say "You lost the game";
-			die;
+			exit;
 		}
 		$board[$hero[0]][$hero[1]] = '.';
 		$board[$hero[0] + 1][$hero[1]] = 'H';
 		@hero[0] = $hero[0] + 1;
+		recover();
 	print_board(@board);
 	}
 	else{
@@ -253,7 +228,7 @@ sub battle{
 	if($board[$coord[0]][$coord[1]] eq 'E')
 	{
 		$enemy = shift @enemies;
-		say "Your enemy is ". $enemy->name . " its health is: " . $enemy->health . "and its power is : " . $enemy->power;
+		say "Your enemy is ". $enemy->name . " its health is: " . $enemy->health . " and its power is : " . $enemy->power;
 		while($enemy->health > 0 && $hero->health > 0)
 		{
 			say "Your health is: " . $hero->health;
@@ -288,13 +263,26 @@ sub take_treasury{
 
 sub recover{
 	if ($hero->health < $hero->start_health){
-		if($hero->health + 20 > $hero->start_health){
+		my $recovery = 5;
+		if($hero->health + $recovery > $hero->start_health){
 			$hero->health($hero->start_health);
 		}
 		else{
-			$hero->health($hero->health + 20);
+			$hero->health($hero->health + $recovery);
 		}
 		say "Now your health is ".$hero->health;
+	} 
+}
+
+sub health{
+	if ($hero->health < $hero->start_health){
+		my $recovery = 20;
+		if($hero->health + $recovery > $hero->start_health){
+			$hero->health($hero->start_health);
+		}
+		else{
+			$hero->health($hero->health + $recovery);
+		}
 	} 
 }
 
@@ -305,3 +293,53 @@ sub take_new_weapon{
 	$hero->weapon_power($new_weapon_power);
 	say "You take ".$hero->weapon." with power ".$hero->weapon_power;
 }
+
+sub won_game{
+	my @coord = @_;
+	if($board[$coord[0]][$coord[1]] eq 'F')
+	{
+		say "You entered this stage!!";
+		exit;
+	}
+}
+
+sub info{
+	say "You are ".$hero->name;
+	say "Your weapon is ".$hero->weapon." with power ".$hero->weapon_power; 	
+	say "Now your health is ".$hero->health;
+}
+
+sub the_game{
+	say "write 'exit' for end of the game";
+	say "Choose level:\neasy\nmedium\nhard\n";
+	my $i = 0;
+	while (<STDIN>) {
+	    chomp;
+
+	    next unless length;
+	    last if $_ =~ /exit/i;
+	    
+	    if ($i == 0)
+	    {
+	    	next unless $_ =~ /easy/i || $_ =~ /medium/i || $_ =~ /hard/i;
+	    	switch($_){
+	    		case 'easy' {$level = 1}
+	    		case 'medium' {$level = 2}
+	    		case 'hard' {$level = 3}
+	    	}
+	    	@board = generate_table();
+	    	print_board(@board);
+	    	$i += 1;
+	    	say "You are ".$hero->name.".";
+	    	say "Try to kill as many as possible enemies.\n";
+	    	say "Type 'help' for more information about commands.";
+	    }
+	    elsif($i > 1){
+	    	next unless defined $commands{$_};
+		    $commands{$_}->();
+		}
+	   	say "Enter command";
+	    $i += 1;
+	}
+}
+the_game();
